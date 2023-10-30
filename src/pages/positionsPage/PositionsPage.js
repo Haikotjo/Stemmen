d3be3729e9fefaa5b91c9cfb385a818d64f953c8
Haikotjo <haikowierda@hotmail.com>
2023-10-30 15:16:26 +0100

    positonsPage code comments en opschonen ook van componenten en hooks

--- src/pages/positionsPage/PositionsPage.js
+++ src/pages/positionsPage/PositionsPage.js
@@ -1,26 +1,41 @@
+// Importing required modules and components
 import React, { useState } from 'react';
 import positionsData from '../../data/positions.json';
 import TopicList from "../../Components/topicList/TopicList";
 import usePositions from "../../hooks/usePositions";
 import PartyPosition from "../../Components/partyPosition/PartyPosition";
 
-//weergeven van de naam van de partij en hun standpunt voor een geselecteerd onderwerp.
+// PositionsPage component definition
+// This component displays the positions of various parties on a selected topic
 function PositionsPage() {
+    // State to keep track of the selected topic
     const [selectedTopic, setSelectedTopic] = useState(null);
+
+    // Custom hook to get the positions of parties on the selected topic
     const positions = usePositions(selectedTopic);
+
+    // Extracting the list of topics from positionsData
     const topics = Object.keys(positionsData);
 
+    // Handler function to set the selected topic
     const handleTopicSelection = (topic) => {
         setSelectedTopic(topic);
     };
 
+    // Render the component
     return (
         <div className="PositionsPage">
+            {/* TopicList component to display the list of topics */}
             <TopicList
                 topics={topics}
                 selectedTopic={selectedTopic}
                 handleTopicSelection={handleTopicSelection}
             />
+
+            {/*
+                Loop through the positions object and render a PartyPosition component for each party.
+                Pass the party name, position, and selected topic as props.
+            */}
             {Object.keys(positions).map((party) => (
                 <PartyPosition key={party} party={party} position={positions[party]} topic={selectedTopic} />
             ))}
@@ -28,4 +43,5 @@ function PositionsPage() {
     );
 }
 
+// Export the component for use in other files
 export default PositionsPage;
