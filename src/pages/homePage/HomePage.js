195fd0818cd4bdbff5e053a45147f3a7d0928a6c
Haikotjo <haikowierda@hotmail.com>
2023-10-31 20:24:44 +0100

    component voor de pagediscription en al wat scss

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import textData from '../../data/textData .json';
+import textData from '../../data/textData.json';
 import logo from '../../assets/images/Stemspectrum.svg';
+import styles from './Hompage.module.scss'
+import PageDescriptionBlock from "../../Components/pageDescriptionBlock/PageDescriptionBlock";
 
 const HomePage = () => {
+    const routesMap = {
+        "positionsPage": "/standpunten-pagina",
+        "partiesPage": "/partij-pagina",
+        "electionHelper": "/kies-hulp"
+    };
+
     return (
-        <div>
-            <img src={logo} alt="StemSpectrum Logo" />
+        <div className={styles.container}>
+            <img src={logo} alt="StemSpectrum Logo" className={styles.logo} />
             <h1>Welkom bij StemSpectrum</h1>
             <p>{textData.nl.introText}</p>
             <h2>Verkiezingsinformatie</h2>
             <p>{textData.nl.electionInfo}</p>
             <h2>Extra Informatie</h2>
             <p>{textData.nl.extraStory}</p>
+            <h2>Pagina Beschrijvingen</h2>
+            {Object.entries(textData.nl.pageDescriptions).map(([key, value]) => (
+                <PageDescriptionBlock key={key} route={routesMap[key]} description={value} />
+            ))}
         </div>
     );
 };
