f2f831909ae578c73186441eb138522e92518c63
Haikotjo <haikowierda@hotmail.com>
2023-11-11 21:40:33 +0100

    Foutje met de priveders. Is gefixed

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -7,31 +7,30 @@ import PositionsPage from "../pages/positionsPage/PositionsPage";
 import NavBar from "../Components/NavBar/NavBar";
 import TestPage from "../pages/testPage/TestPage";
 import { ScoreProvider } from '../context/ScoreContext';
-import { LanguageProvider } from '../contexts/LanguageContext'; // Voeg je taalcontext toe
-import { ThemeProvider } from '../contexts/ThemeContext'; // Voeg je themacontext toe
-import { ModeProvider } from '../contexts/ModeContext'; // Voeg je moduscontext toe
+import {LanguageProvider} from "../context/LanguageContext";
+import {ThemeProvider} from "../context/ThemeContext";
+import {ModeProvider} from "../context/ModeContext";
 
 function AppRoutes() {
     return (
-        <LanguageProvider> {/* Taalcontext omhult alles */}
-            <NavBar /> {/* NavBar buiten ThemeProvider */}
-            <ThemeProvider> {/* Themacontext omhult alleen de Routes */}
-                <ModeProvider> {/* Moduscontext omhult ook de Routes */}
-                    <Router>
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/kies-hulp" element={<ElectionHelper />} />
-                            <Route path="/partij-pagina" element={<PartiesPage />} />
-                            <Route path="/standpunten-pagina" element={<PositionsPage />} />
-                            <Route path="/testpagina" element={<TestPage />} />
-                            {/* Voeg hier meer routes toe */}
-                        </Routes>
-                    </Router>
-                </ModeProvider>
-            </ThemeProvider>
-            <ScoreProvider> {/* ScoreProvider kan hier of binnen ThemeProvider afhankelijk van de vereisten */}
-                {/* Optioneel: Andere componenten of routes die ScoreProvider vereisen */}
-            </ScoreProvider>
+        <LanguageProvider>
+            <Router>
+                <NavBar />
+                <ScoreProvider> {/* ScoreProvider alleen voor de Routes */}
+                    <ThemeProvider>
+                        <ModeProvider>
+                            <Routes>
+                                <Route path="/" element={<HomePage />} />
+                                <Route path="/kies-hulp" element={<ElectionHelper />} />
+                                <Route path="/partij-pagina" element={<PartiesPage />} />
+                                <Route path="/standpunten-pagina" element={<PositionsPage />} />
+                                <Route path="/testpagina" element={<TestPage />} />
+                                {/* Meer routes */}
+                            </Routes>
+                        </ModeProvider>
+                    </ThemeProvider>
+                </ScoreProvider>
+            </Router>
         </LanguageProvider>
     );
 }
