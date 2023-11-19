b747b3d925b3009de34a9da0c8da2fdeebf83928
Haikotjo <haikowierda@hotmail.com>
2023-11-19 18:30:08 +0100

    ScorePage netter gemaakt. Niet tevreden maar het lukt me echt niet om de tekst anders te verwerken

--- src/Components/pageHeader/PageHeader.js
+++ src/Components/pageHeader/PageHeader.js
@@ -1,11 +1,18 @@
-// PageHeader.jsx
 import React from 'react';
 import styles from './PageHeader.module.scss';
 
+// PageHeader Component
+// This component is used for displaying a page header with an image and a title.
+// Props:
+//   imageSrc: The source URL of the header image.
+//   title: The title text to be displayed in the header.
 const PageHeader = ({ imageSrc, title }) => {
     return (
         <div className={styles.headerWrapper}>
+            {/* Header Image: Displays the image passed through the imageSrc prop */}
             <img src={imageSrc} alt="Header" className={styles.backgroundImage} />
+
+            {/* Header Text: Displays the title passed through the title prop */}
             <h1 className={styles.headerText}>{title}</h1>
         </div>
     );
--- src/pages/scorePage/ScorePage.js
+++ src/pages/scorePage/ScorePage.js
@@ -1,4 +1,3 @@
-
 import React, { useContext } from 'react';
 import { ScoreContext } from '../../context/ScoreContext';
 import styles from './ScorePage.module.scss';
@@ -9,15 +8,23 @@ import StyledButton from "../../Components/button/StyledButton";
 import useReset from "../../hooks/useReset";
 
 const ScorePage = () => {
+    // Accessing party scores from the context
     const { partyScores } = useContext(ScoreContext);
+    // Using custom hook to access the current language setting
     const { language } = useLanguage();
+    // Custom hook for handling reset functionality
     const handleReset = useReset();
+    // Retrieving saved data from local storage
     const savedData = JSON.parse(localStorage.getItem('answeredQuestions'));
+    // Retrieving saved language preference from local storage
     const savedLanguage = localStorage.getItem('language');
+    // Array to hold parties with which the user agrees or disagrees
     const partiesUserAgreesOrDisagreesWith = [];
 
+    // Looping through saved data to extract user's answers
     for (const [questionPartyKey, answer] of Object.entries(savedData)) {
         const [topic, party] = questionPartyKey.split('_');
+        // Filtering answers based on language and pushing them into the array
         if (language === 'nl' && (answer === 'Eens' || answer === 'Oneens' || answer === 'Neutraal')) {
             partiesUserAgreesOrDisagreesWith.push({ topic, party, answer });
         } else if (language === 'kids' && (answer === 'Ja goed plan!' || answer === 'Nee joh!' || answer === 'Ik weet niet')) {
@@ -26,7 +33,6 @@ const ScorePage = () => {
             partiesUserAgreesOrDisagreesWith.push({ topic, party, answer });
         }
     }
-
 
     const translations = {
         nl: {
@@ -46,13 +52,17 @@ const ScorePage = () => {
         },
     };
 
+    // Retrieving the correct translation based on saved language
     const t = translations[savedLanguage];
 
+    // Sorting party scores in descending order
     const sortedPartyScores = Object.entries(partyScores).sort(([, scoreA], [, scoreB]) => scoreB - scoreA);
+    // Mapping sorted scores to include image source and empty answers array
     const partiesWithImages = sortedPartyScores.map(([party, score]) => ({
         party, score, imageSrc: getPartyImage(party), answers: []
     }));
 
+    // Adding user's answers to corresponding parties
     for (const { topic, party, answer } of partiesUserAgreesOrDisagreesWith) {
         const partyIndex = partiesWithImages.findIndex(partyObj => partyObj.party === party);
         if (partyIndex !== -1) {
@@ -60,6 +70,7 @@ const ScorePage = () => {
         }
     }
 
+    // Check if there are any scores to display
     const hasScores = Object.keys(partyScores).length > 0;
 
     return (
@@ -104,16 +115,16 @@ const ScorePage = () => {
                 ) : (
                     <p>{t.noScores} <Link to="/kies-hulp" className={styles.linkToPage}>&gt;&gt;&gt;</Link></p>
                 )}
-            </div>
-            <div className={styles.buttonsContainer}>
-                {hasScores && (
+                <div className={styles.buttonsContainer}>
+                    {hasScores && (
+                        <Link to='/kies-hulp'>
+                            <StyledButton label="Reset" onClick={handleReset} />
+                        </Link>
+                    )}
                     <Link to='/kies-hulp'>
-                        <StyledButton label="Reset" onClick={handleReset} />
+                        <StyledButton label={t.electionHelp} />
                     </Link>
-                )}
-                <Link to='/kies-hulp'>
-                    <StyledButton label={t.electionHelp} />
-                </Link>
+                </div>
             </div>
         </>
     );
