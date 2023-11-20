dd596f94cf682ed60782e8409847c72f4468cefe
Haikotjo <haikowierda@hotmail.com>
2023-11-20 15:19:35 +0100

    hoop codeComments toegevoegd

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
-import styles from './Footer.module.scss'; // Importeer de SCSS-module
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa'; // Importing icons from react-icons
+import styles from './Footer.module.scss'; // Importing SCSS module for styling
 
+// Define the Footer functional component
 const Footer = () => {
     return (
+        // Render a footer element with a class from the SCSS module
         <footer className={styles.footer}>
 
             <div className={styles.icons}>
+                {/* Display a footer text */}
                 <p className={styles.footerText}> By Haiko Wierda</p>
+
+                {/* Link to LinkedIn profile */}
+                {/* Opens in a new tab and uses rel="noopener noreferrer" for security */}
                 <a href="https://www.linkedin.com/in/haiko-wierda-89aa7412a/" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin />
+                    <FaLinkedin /> {/* LinkedIn icon */}
                 </a>
+
+                {/* Link to GitHub profile */}
+                {/* Opens in a new tab and uses rel="noopener noreferrer" for security */}
                 <a href="https://github.com/Haikotjo" target="_blank" rel="noopener noreferrer">
-                    <FaGithub />
+                    <FaGithub /> {/* GitHub icon */}
                 </a>
+
+                {/* Mailto link for sending an email */}
                 <a href="mailto:haikowierdaklusjes@gmail.com">
-                    <FaEnvelope />
+                    <FaEnvelope /> {/* Envelope icon for email */}
                 </a>
             </div>
         </footer>
     );
 };
 
-export default Footer;
+export default Footer; // Export the Footer component for use in other parts of the application
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,85 +1,76 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './NavBar.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import HamburgerMenu from "../hamburgerMenu/HamburgerMenu";
-import { useLanguage } from "../../context/LanguageContext";
-import LanguageMenu from "../languageMenu/LanguageMenu";
+import { NavLink } from 'react-router-dom'; // Import NavLink for navigation links
+import styles from './NavBar.module.scss'; // Import SCSS module for styling
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesome for icons
+import { faHome } from '@fortawesome/free-solid-svg-icons'; // Import home icon
+import HamburgerMenu from "../hamburgerMenu/HamburgerMenu"; // Import HamburgerMenu component
+import { useLanguage } from "../../context/LanguageContext"; // Import language context hook
+import LanguageMenu from "../languageMenu/LanguageMenu"; // Import LanguageMenu component
+import textData from "../../data/textData.json"; // Import text data for different languages
 
+// Define the NavBar functional component
 function NavBar() {
+    const { language } = useLanguage(); // Retrieve current language from context
+    const currentTextData = textData[language]; // Get text data for current language
 
-    const { language } = useLanguage();
-
-    // Object met linkteksten voor beide talen
+    // Object containing link texts for different languages
     const linkTexts = {
-        nl: {
-            kiesHulp: "Kies Hulp",
-            partijPagina: "De Partijen",
-            standpuntenPagina: "Standpunten",
-            scorePage: "Mijn Partij Match"
-        },
-        en: {
-            kiesHulp: "Election Helper",
-            partijPagina: "Party Page",
-            standpuntenPagina: "Positions Page",
-            scorePage: "Best Match"
-        },
-        kids: {
-            kiesHulp: "Kies Hulpje",
-            partijPagina: "De Partijen",
-            standpuntenPagina: "Standpunten",
-            scorePage: "Beste Partijen"
-        },
+        // ... language text definitions
     };
-
-    const currentLinkTexts = linkTexts[language];
+    const currentLinkTexts = linkTexts[language]; // Retrieve texts for the current language
 
     return (
+        // Navbar container
         <div className={styles.navbar}>
             <div className={styles.container}>
+                {/* Home icon with navigation link */}
                 <div className={styles.icon}>
                     <NavLink to="/">
                         <FontAwesomeIcon icon={faHome} color="white" size="2x" />
                     </NavLink>
                 </div>
+                {/* Links for different pages */}
                 <div className={styles.links}>
+                    {/* NavLink used for navigation, with styles for active and default states */}
+                    {/* Dynamically rendered based on language */}
                     <NavLink
                         to="/kies-hulp"
                         className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}
                     >
-                        {currentLinkTexts.kiesHulp}
+                        {currentTextData.pages.electionHelper.name}
                     </NavLink>
 
                     <NavLink
                         to="/partij-pagina"
                         className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}
                     >
-                        {currentLinkTexts.partijPagina}
+                        {currentTextData.pages.partiesPage.name}
                     </NavLink>
 
                     <NavLink
                         to="/standpunten-pagina"
                         className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}
                     >
-                        {currentLinkTexts.standpuntenPagina}
+                        {currentTextData.pages.positionsPage.name}
                     </NavLink>
 
                     <NavLink
                         to="/score-page"
                         className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link}
                     >
-                        {currentLinkTexts.scorePage}
+                        {currentTextData.pages.scorePage.name}
                     </NavLink>
 
                 </div>
+                {/* Hamburger menu for smaller screens */}
                 <div className={styles.hamburgerMenu}>
                     <HamburgerMenu />
                 </div>
+                {/* Language selection menu */}
                 <LanguageMenu label='Language'/>
             </div>
         </div>
     );
 }
 
-export default NavBar;
+export default NavBar; // Export the component
